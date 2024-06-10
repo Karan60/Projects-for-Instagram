f8e41332f838e7ecc32011e3f3a37dbe3cae5c6d
KARAN NATH <112185032+Karan60@users.noreply.github.com>
2024-06-10 21:51:50 +0530

    done

---
+++ 17.Image slider/script.js
@@ -0,0 +1,12 @@
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
